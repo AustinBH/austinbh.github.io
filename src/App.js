878a108f1f45c586821ab388a4c3e778517d8521
AustinBH <harlowaustin2@gmail.com>
2019-08-23 17:22:31 -0700

    added initial portfolio setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import headshot from './images/headshot.JPG'
+import finalFMSample from './images/finalFMSample.png'
+import myTrailsSample from './images/myTrailsSample.png'
+import pokemonTeamsSample from './images/pokemonTeamsSample.png'
 import './App.css';
+import { Image, Header, Grid } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Header as='h1' dividing content='Austin Harlow' />
+        <p>Full Stack Developer</p>
+      <Header as='h1' dividing content='Projects' />
+      <Grid columns={3} divided>
+        <Grid.Row>
+          <a href='https://my-trails.herokuapp.com' target='_blank' rel='noopener noreferrer'>
+            <Grid.Column>
+              <Image className='project-image' src={myTrailsSample} size='medium' />
+            </Grid.Column>
+          </a>
+          <a href='https://the-final-fm.herokuapp.com' target='_blank' rel='noopener noreferrer'>
+            <Grid.Column>
+              <Image className='project-image' src={finalFMSample} size='medium' />
+            </Grid.Column>
+          </a>
+          <a href='https://austinbh.github.io/Pokedex-Teams/' target='_blank' rel='noopener noreferrer'>
+            <Grid.Column>
+              <Image className='project-image' src={pokemonTeamsSample} size='medium' />
+            </Grid.Column>
+          </a>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
