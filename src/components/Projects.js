e1075115c63c7ac47668c593f3fda86327d5114f
AustinBH <harlowaustin2@gmail.com>
2019-08-25 11:59:00 -0700

    updated button size to format a bit better on mobile still w.i.p

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -59,26 +59,26 @@ const Projects = props => {
             <Grid.Row>
                 <Grid.Column>
                     <a href='https://github.com/AustinBH/my-trails-frontend' target='_blank' rel='noopener noreferrer'>
-                        <Button className='project-button' color='black' icon='github' content='Frontend' />
+                        <Button compact className='project-button' color='black' icon='github' content='Frontend' />
                     </a>
                     <a href='https://github.com/AustinBH/my-trails-backend' target='_blank' rel='noopener noreferrer'>
-                        <Button className='project-button' color='black' icon='github' content='Backend' />
+                        <Button compact className='project-button' color='black' icon='github' content='Backend' />
                     </a>
                 </Grid.Column>
                 <Grid.Column>
                     <a href='https://github.com/AustinBH/the-final-fm-frontend' target='_blank' rel='noopener noreferrer'>
-                        <Button color='black' icon='github' content='Frontend' />
+                        <Button compact color='black' icon='github' content='Frontend' />
                     </a>
                     <a href='https://github.com/AustinBH/the-final-fm-backend' target='_blank' rel='noopener noreferrer'>
-                        <Button color='black' icon='github' content='Backend' />
+                        <Button compact color='black' icon='github' content='Backend' />
                     </a>
                 </Grid.Column>
                 <Grid.Column>
                     <a href='https://github.com/AustinBH/pokedex-teams' target='_blank' rel='noopener noreferrer'>
-                        <Button color='black' icon='github' content='Frontend' />
+                        <Button compact color='black' icon='github' content='Frontend' />
                     </a>
                     <a href='https://github.com/AustinBH/pokedex-backend' target='_blank' rel='noopener noreferrer'>
-                        <Button color='black' icon='github' content='Backend' />
+                        <Button compact color='black' icon='github' content='Backend' />
                     </a>
                 </Grid.Column>
             </Grid.Row>
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 
 const SocialMedia = props => {
-    return <>
-            <a
-                href='https://docs.google.com/document/d/1ZhfCdV35E5LYpiLtYayQuO2wdbALZLm-UZ7V_AdJVgI/'
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                <Button className='contact-button' icon='file pdf' color='blue' content='Resume'/>
-            </a>
-            <a href='https://github.com/AustinBH' target='_blank' rel='noopener noreferrer'>
-                <Button className='contact-button' icon='github' color='black' content='Github' />
-            </a>
-            <a href='https://www.linkedin.com/in/austin-harlow/' target='_blank' rel='noopener noreferrer'>
-                <Button className='contact-button' icon='linkedin' color='linkedin' content='LinkedIn' />
-            </a>
-            <a href='https://dev.to/austinbh' target='_blank' rel='noopener noreferrer'>
-                <Button className='contact-button' icon='book' color='green' content='Blog' />
-            </a>
-            <p>Email: <a href='mailto:harlowaustin2@gmail.com'>harlowaustin2@gmail.com</a></p>
-    </>
+    return <Grid columns={1} widths='equal'>
+        <Grid.Row>
+            <Grid.Column width={16}>
+                <a
+                    href='https://docs.google.com/document/d/1ZhfCdV35E5LYpiLtYayQuO2wdbALZLm-UZ7V_AdJVgI/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <Button compact className='contact-button' icon='file pdf' color='blue' content='Resume' />
+                </a>
+                <a href='https://github.com/AustinBH' target='_blank' rel='noopener noreferrer'>
+                    <Button compact className='contact-button' icon='github' color='black' content='Github' />
+                </a>
+                <a href='https://www.linkedin.com/in/austin-harlow/' target='_blank' rel='noopener noreferrer'>
+                    <Button compact className='contact-button' icon='linkedin' color='linkedin' content='LinkedIn' />
+                </a>
+                <a href='https://dev.to/austinbh' target='_blank' rel='noopener noreferrer'>
+                    <Button compact className='contact-button' icon='book' color='green' content='Blog' />
+                </a>
+            </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+            <Grid.Column textAlign='center' width={16}>
+                <p>Email: <a href='mailto:harlowaustin2@gmail.com'>harlowaustin2@gmail.com</a></p>
+            </Grid.Column>
+        </Grid.Row>
+    </Grid>
 }
 
 export default SocialMedia;
