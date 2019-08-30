b746686dd670352f57142c72d406529af9150ab3
AustinBH <harlowaustin2@gmail.com>
2019-08-29 22:26:41 -0700

    updated title/resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -21,7 +21,7 @@ const Resume = props => {
             <Icon name='file pdf' circular size='mini' />
             <Header.Content>Resume</Header.Content>
         </Header>
-        <p>Full stack software developer with a passion for creative work in a fast-paced collaborative work environment. 
+        <p>Full Stack Software Engineer with a passion for creative work in a fast-paced collaborative work environment. 
         With experience in Ruby on Rails, JavaScript, and React and a background in customer service, I discovered web development through my friends and coworkers at Amazon.
         I grew up hiking all over the American West, and I bring the same tenacity and creativity to every project and assignment that I developed when learning how to live on nothing but what fish I could catch and what I had in my pack.</p>
         <TechnicalSkills/>
