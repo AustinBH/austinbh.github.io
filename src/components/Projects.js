44add5c5a9b63b8c14245755c3462faa111fc0ed
AustinBH <harlowaustin2@gmail.com>
2019-09-03 15:09:15 -0700

    updated photos and styled projects page for mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,33 +24,26 @@ const Projects = props => {
             <Grid.Row>
                 <Grid.Column>
                     <Header as='h3' content='My Trails' />
+                    <Image className='project-image' src={myTrailsSample} alt='My Trails' href='https://my-trails.info' target='_blank' rel='noopener noreferrer'/>
+                    <Button compact className='project-button' color='black' icon='github' content='Frontend' href='https://github.com/AustinBH/my-trails-frontend' target='_blank' rel='noopener noreferrer' />
+                    <Button compact className='project-button' color='black' icon='github' content='Backend' href='https://github.com/AustinBH/my-trails-backend' target='_blank' rel='noopener noreferrer' />
                 </Grid.Column>
                 <Grid.Column>
                     <Header as='h3' content='The Final FM' />
+                    <Image className='project-image' src={finalFMSample} alt='Final FM' href='https://the-final-fm.herokuapp.com' target='_blank' rel='noopener noreferrer'/>
+                    <Button compact className='project-button' color='black' icon='github' content='Frontend' href='https://github.com/AustinBH/the-final-fm-frontend' target='_blank' rel='noopener noreferrer' />
+                    <Button compact className='project-button' color='black' icon='github' content='Backend' href='https://github.com/AustinBH/the-final-fm-backend' target='_blank' rel='noopener noreferrer' />
                 </Grid.Column>
                 <Grid.Column>
                     <Header as='h3' content='Pokemon Teams' />
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column>
-                    <a href='https://my-trails.info' target='_blank' rel='noopener noreferrer'>
-                        <Image className='project-image' src={myTrailsSample} size='medium' alt='My Trails'/>
-                    </a>
-                </Grid.Column>
-                <Grid.Column>
-                    <a href='https://the-final-fm.herokuapp.com' target='_blank' rel='noopener noreferrer'>
-                        <Image className='project-image' src={finalFMSample} size='medium' alt='Final FM'/>
-                    </a>
-                </Grid.Column>
-                <Grid.Column>
-                    <a href='https://austinbh.github.io/Pokedex-Teams/' target='_blank' rel='noopener noreferrer'>
-                        <Image className='project-image' src={pokemonTeamsSample} size='medium' alt='Pokemon Teams'/>
-                    </a>
+                    <Image className='project-image' src={pokemonTeamsSample} alt='Pokemon Teams' href='https://austinbh.github.io/Pokedex-Teams/' target='_blank' rel='noopener noreferrer'/>
+                    <Button compact className='project-button' color='black' icon='github' content='Frontend' href='https://github.com/AustinBH/pokedex-teams' target='_blank' rel='noopener noreferrer' />
+                    <Button compact className='project-button' color='black' icon='github' content='Backend' href='https://github.com/AustinBH/pokedex-backend' target='_blank' rel='noopener noreferrer' />
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column className='project-info'>
+                    <Header as='h3' content='My Trails' className='project-info-header' />
                     <p><em>A trail finding app that uses the Google Maps API to display maps with trail info.</em></p>
                     <p>Technologies Used:</p>
                     <List>
@@ -59,12 +52,14 @@ const Projects = props => {
                         <List.Item icon='map marker alternate' content='Geolocation API' />
                         <List.Item icon='registered' content='Redux' />
                         <List.Item icon='react' content='React' />
+                        <List.Item icon='gem' content='Ruby on Rails' />
                         <List.Item icon='speakap' content='Semantic UI React' />
-                        <List.Item icon='css3' content='CSS3' />
                         <List.Item icon='html5' content='HTML5' />
+                        <List.Item icon='css3' content='CSS3' />
                     </List>
                 </Grid.Column>
                 <Grid.Column className='project-info'>
+                    <Header as='h3' content='The Final FM' className='project-info-header' />
                     <p><em>A song and concert search app that allows users to save their favorite songs.</em></p>
                     <p>Technologies Used:</p>
                     <List>
@@ -77,6 +72,7 @@ const Projects = props => {
                     </List>
                 </Grid.Column>
                 <Grid.Column className='project-info'>
+                    <Header as='h3' content='Pokemon Teams' className='project-info-header' />
                     <p><em>A Pokedex app for fans of Pokemon to create teams using their favorite Pokemon.</em></p>
                     <p>Technologies Used:</p>
                     <List>
@@ -85,20 +81,6 @@ const Projects = props => {
                         <List.Item icon='html5' content='HTML5' />
                         <List.Item icon='css3' content='CSS3' />
                     </List>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column>
-                    <Button compact className='project-button' color='black' icon='github' content='Frontend' href='https://github.com/AustinBH/my-trails-frontend' target='_blank' rel='noopener noreferrer' />
-                    <Button compact className='project-button' color='black' icon='github' content='Backend' href='https://github.com/AustinBH/my-trails-backend' target='_blank' rel='noopener noreferrer' />
-                </Grid.Column>
-                <Grid.Column>
-                    <Button compact className='project-button' color='black' icon='github' content='Frontend' href='https://github.com/AustinBH/the-final-fm-frontend' target='_blank' rel='noopener noreferrer'/>
-                    <Button compact className='project-button' color='black' icon='github' content='Backend' href='https://github.com/AustinBH/the-final-fm-backend' target='_blank' rel='noopener noreferrer'/>
-                </Grid.Column>
-                <Grid.Column>
-                    <Button compact className='project-button' color='black' icon='github' content='Frontend' href='https://github.com/AustinBH/pokedex-teams' target='_blank' rel='noopener noreferrer'/>
-                    <Button compact className='project-button' color='black' icon='github' content='Backend' href='https://github.com/AustinBH/pokedex-backend' target='_blank' rel='noopener noreferrer'/>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
