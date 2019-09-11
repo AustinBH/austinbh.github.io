7f221b55753fea77f584848479cb8348641810ca
AustinBH <harlowaustin2@gmail.com>
2019-09-11 09:39:18 -0700

    added python and updated technical skills to have a popup which displays language info

--- src/components/ResumeAdditions/TechnicalSkills.js
+++ src/components/ResumeAdditions/TechnicalSkills.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Divider, Grid, Image, Header, Icon } from 'semantic-ui-react';
+import { Divider, Grid, Image, Header, Icon, Popup } from 'semantic-ui-react';
 import Bootstrap from '../../images/skills/Bootstrap_logo.svg'
 import Ruby from '../../images/skills/Ruby_logo.svg'
 import Rails from '../../images/skills/Ruby_on_Rails_logo.svg'
@@ -9,8 +9,10 @@ import JS from '../../images/skills/Unofficial_JavaScript_logo_2.svg'
 import ReactLogo from '../../logo.svg'
 import Redux from '../../images/skills/Redux_logo.svg'
 import Postgres from '../../images/skills/Postgresql_elephant.svg'
+import Python from '../../images/skills/Python_logo.svg'
 
 const TechnicalSkills = props => {
+
     return <>
         <Divider horizontal>
             <Header as='h3'>
@@ -18,49 +20,121 @@ const TechnicalSkills = props => {
                 Technical Skills
             </Header>
         </Divider>
-        <Grid columns={5} celled='internally' textAlign='center'>
+        <Grid columns={4} celled='internally' textAlign='center'>
             <Grid.Row>
                 <Grid.Column>
-                    <Image src={Ruby} alt='Ruby logo' className='skills-image' centered />
-                    <p className='skills-text'>Ruby</p>
+                    <Popup
+                        size='tiny'
+                        position='top right'
+                        content='Ruby'
+                        on='click'
+                        pinned
+                        trigger={<Image src={Ruby} alt='Ruby logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src={Rails} alt='Ruby on Rails logo' className='skills-image' centered/>
-                    <p className='skills-text'>Ruby on Rails</p>
+                    <Popup
+                        size='tiny'
+                        position='top center'
+                        content='Ruby on Rails'
+                        on='click'
+                        pinned
+                        trigger={<Image src={Rails} alt='Ruby on Rails logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src={HTML} alt='HTML5 logo' className='skills-image' centered/>
-                    <p className='skills-text'>HTML5</p>
+                    <Popup
+                        size='tiny'
+                        position='top center'
+                        content='HTML5'
+                        on='click'
+                        pinned
+                        trigger={<Image src={HTML} alt='HTML5 logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src={CSS} alt='CSS3 logo' className='skills-image' centered/>
-                    <p className='skills-text'>CSS3</p>
-                </Grid.Column>
-                <Grid.Column>
-                    <Image src={JS} alt='JavaScript logo' className='skills-image' centered/>
-                    <p className='skills-text'>JavaScript</p>
+                    <Popup
+                        size='tiny'
+                        position='top left'
+                        content='CSS3'
+                        on='click'
+                        pinned
+                        trigger={<Image src={CSS} alt='CSS3 logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column>
-                    <Image src={ReactLogo} alt='React logo' className='skills-image' centered/>
-                    <p className='skills-text'>React</p>
+                    <Popup
+                        size='tiny'
+                        position='bottom right'
+                        content='JavaScript'
+                        on='click'
+                        pinned
+                        trigger={<Image src={JS} alt='JavaScript logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src={Redux} alt='Redux logo' className='skills-image' centered/>
-                    <p className='skills-text'>Redux</p>
+                    <Popup
+                        size='tiny'
+                        position='bottom right'
+                        content='React'
+                        on='click'
+                        pinned
+                        trigger={<Image src={ReactLogo} alt='React logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src='https://react.semantic-ui.com/logo.png' alt='Semantic UI logo' className='skills-image' centered/>
-                    <p className='skills-text'>Semantic UI</p>
+                    <Popup
+                        size='tiny'
+                        position='bottom left'
+                        content='Redux'
+                        on='click'
+                        pinned
+                        trigger={<Image src={Redux} alt='Redux logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src={Bootstrap} alt='Bootstrap logo' className='skills-image' centered/>
-                    <p className='skills-text'>Bootstrap</p>
+                    <Popup
+                        size='tiny'
+                        position='bottom left'
+                        content='Semantic UI'
+                        on='click'
+                        pinned
+                        trigger={<Image src='https://react.semantic-ui.com/logo.png' alt='Semantic UI logo' className='skills-image' centered />}
+                    />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column>
+                    <Popup
+                        size='tiny'
+                        position='bottom center'
+                        content='Bootstrap'
+                        on='click'
+                        pinned
+                        trigger={<Image src={Bootstrap} alt='Bootstrap logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
                 <Grid.Column>
-                    <Image src={Postgres} alt='Postgresql logo' className='skills-image' centered/>
-                    <p className='skills-text'>Postgresql</p>
+                    <Popup
+                        size='tiny'
+                        position='bottom center'
+                        content='PostgreSQL'
+                        on='click'
+                        pinned
+                        trigger={<Image src={Postgres} alt='Postgresql logo' className='skills-image' centered />}
+                    />
+                </Grid.Column>
+                <Grid.Column>
+                    <Popup
+                        size='tiny'
+                        position='bottom center'
+                        content='Python'
+                        on='click'
+                        pinned
+                        trigger={<Image src={Python} alt='Python logo' className='skills-image' centered />}
+                    />
                 </Grid.Column>
             </Grid.Row>
         </Grid>
