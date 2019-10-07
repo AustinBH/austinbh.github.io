de53fb2cb4a777065ebbcf3fa7582532dc707cd3
AustinBH <harlowaustin2@gmail.com>
2019-10-07 13:41:10 -0700

    added transition for sidenav and updated links

--- src/components/InfoMenu.js
+++ src/components/InfoMenu.js
@@ -10,20 +10,19 @@ import { blogs } from './blogs/BlogHolder';
 const InfoMenu = props => {
 
     return <Router>
-            <Sidebar
-                as={Menu}
-                animation='overlay'
-                icon='labeled'
-                inverted
-                vertical
-                visible
-                width='thin'
-            >
-                <Menu.Item icon='map' as={NavLink} exact to='/about' activeClassName='active' name='About' />
-                <Menu.Item icon='file code' as={NavLink} exact to='/projects' activeClassName='active' name='Projects' />
-                <Menu.Item icon='file alternate' as={NavLink} exact to='/resume' activeClassName='active' name='Resume' />
-                <Menu.Item icon='book' as={NavLink} exact to={`/blog/${blogs.length}`} activeClassName='active' name='Blog' />
-            </Sidebar>
+                <Sidebar
+                    as={Menu}
+                    icon='labeled'
+                    inverted
+                    vertical
+                    visible
+                >
+                    <Menu.Item icon='home' as={NavLink} exact to='/' activeClassName='active' name='Home'/>
+                    <Menu.Item icon='map' as={NavLink} exact to='/about' activeClassName='active' name='About' />
+                    <Menu.Item icon='file code' as={NavLink} exact to='/projects' activeClassName='active' name='Projects' />
+                    <Menu.Item icon='file alternate' as={NavLink} exact to='/resume' activeClassName='active' name='Resume' />
+                    <Menu.Item icon='book' as={NavLink} exact to={`/blog/${blogs.length}`} activeClassName='active' name='Blog' />
+                </Sidebar>
                 <Switch>
                     <Route path='/' exact render={null} />
                     <Route path='/about' exact render={props => <About {...props} />} />
