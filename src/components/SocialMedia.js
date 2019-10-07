578896403f9aa48402831be6ac18df02002b0608
AustinBH <harlowaustin2@gmail.com>
2019-10-07 13:56:46 -0700

    updated contact buttons to have same class as project buttons (gives space on mobile)

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,13 +5,9 @@ const SocialMedia = props => {
     return <Grid columns={1} widths='equal' verticalAlign='middle'>
         <Grid.Row>
             <Grid.Column width={16}>
-                <Button className='contact-button' icon='github' color='black' content='Github' href='https://github.com/AustinBH' target='_blank' rel='noopener noreferrer'/>
-                <Button className='contact-button' icon='linkedin' color='linkedin' content='LinkedIn' style={{ color: 'black' }} href='https://www.linkedin.com/in/austin-harlow/' target='_blank' rel='noopener noreferrer'/>
-            </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-            <Grid.Column width={16}>
-                <Button className='contact-button' icon='telegram plane' color='vk' content='Email' href='mailto:harlowaustin2@gmail.com'/>
+                <Button className='project-button' icon='github' color='black' content='Github' href='https://github.com/AustinBH' target='_blank' rel='noopener noreferrer'/>
+                <Button className='project-button' icon='linkedin' color='linkedin' content='LinkedIn' style={{ color: 'black' }} href='https://www.linkedin.com/in/austin-harlow/' target='_blank' rel='noopener noreferrer'/>
+                <Button className='project-button' icon='telegram plane' color='vk' content='Email' href='mailto:harlowaustin2@gmail.com'/>
             </Grid.Column>
         </Grid.Row>
     </Grid>
