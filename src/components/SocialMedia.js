b62783530974e7093efda850326acf0ff3b533d7
AustinBH <harlowaustin2@gmail.com>
2019-11-05 07:58:07 -0800

    added a few small css animations, button bounce + color swap

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,9 +3,9 @@ import { Button } from 'semantic-ui-react';
 
 const SocialMedia = props => {
     return <>
-        <Button className='project-button' icon='github' color='black' content='Github' href='https://github.com/AustinBH' target='_blank' rel='noopener noreferrer'/>
-        <Button className='project-button' icon='linkedin' color='linkedin' content='LinkedIn' style={{ color: 'black' }} href='https://www.linkedin.com/in/austin-harlow/' target='_blank' rel='noopener noreferrer'/>
-        <Button className='project-button' icon='telegram plane' color='vk' content='Email' href='mailto:harlowaustin2@gmail.com'/>
+        <Button className='media-button' icon='github' color='black' content='Github' href='https://github.com/AustinBH' target='_blank' rel='noopener noreferrer'/>
+        <Button className='media-button' icon='linkedin' color='linkedin' content='LinkedIn' style={{ color: 'black' }} href='https://www.linkedin.com/in/austin-harlow/' target='_blank' rel='noopener noreferrer'/>
+        <Button className='media-button' icon='telegram plane' color='vk' content='Email' href='mailto:harlowaustin2@gmail.com'/>
     </>
 }
 
